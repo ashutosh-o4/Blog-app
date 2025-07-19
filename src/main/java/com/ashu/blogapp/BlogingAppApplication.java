6776131a69cc0213a494388987f92b2f5570178e
ashutosh o4 <ashutoshsaklani28@gmail.com>
2025-07-19 13:37:28 +0530

    Initial commit

---
+++ src/main/java/com/ashu/blogapp/BlogingAppApplication.java
@@ -0,0 +1,13 @@
+package com.ashu.blogapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ashu/blogapp/BlogingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ashu.blogapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
